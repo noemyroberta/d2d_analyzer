ec0c374b0b8295dbf119f397e62c8c41dd524e17
Noemy Roberta <noemy.oliveira2@outlook.com>
2024-02-15 20:23:35 -0300

    feat: add `executeLexer()` function to get lexemes

--- file_input/file_input.dart
+++ file_input/file_input.dart
@@ -1,12 +1,35 @@
 import 'dart:async';
 import 'dart:io';
 
+import '../d2d_analyzer.dart';
+
 Future<void> main() async {
   try {
-    var file = File('file_input/file.txt');
-    var contents = await file.readAsString();
-    print(contents);
+   // var file = File('file_input/file.txt');
+    //var contents = await file.readAsString();
+
+    final input = '''
+    class MyClass {
+      int x;
+      String name;
+    }
+  ''';
+    print(input);
+    executeLexer(input);
   } catch (e) {
     print('Error reading file: $e');
   }
+}
+
+void executeLexer(String fileInput) {
+  final lexer = Lexer(fileInput);
+  Token? token;
+  do {
+    token = lexer.getNextToken();
+    if (token != null && token.type != TokenType.INVALID) {
+      print(token);
+    } else if (token != null) {
+      print('Invalid token: ${token.lexeme}');
+    }
+  } while (token != null);
 }
--- lexer/tokens_enum.dart
+++ lexer/tokens_enum.dart
@@ -6,6 +6,7 @@ enum TokenType {
   OPEN_BRACE,
   CLOSE_BRACE,
   SEMICOLON,
+  INVALID,
 }
 
 class Token {
